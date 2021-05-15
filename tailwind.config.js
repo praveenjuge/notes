64a4479b3f3d44a67bb701d6d02d46ce2242a131
praveenjuge <praveen@skcript.com>
2021-05-15 11:50:19 +0530

    Notes
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,5 @@ module.exports = {
     enabled: process.env.HUGO_ENVIRONMENT === "production",
     content: ["./layouts/**/*.html"],
   },
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Inter var",
-          ...require("tailwindcss/defaultTheme").fontFamily.sans,
-        ],
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("tailwindcss/plugin")(function ({ addBase }) {
-      addBase({ ".type-alt": { "font-feature-settings": '"salt"' } });
-    }),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
