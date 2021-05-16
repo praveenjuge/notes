62a938a67b2ac1dd53ba9e2bf2f82209c5c57e8b
praveenjuge <praveen@skcript.com>
2021-05-16 12:16:05 +0530

    Notes
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  purge: {
-    enabled: process.env.HUGO_ENVIRONMENT === "production",
-    content: ["./layouts/**/*.html"],
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
